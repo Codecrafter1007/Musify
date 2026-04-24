3339ab8dc912266194e8aba2bac32e579acfa6f4
Amish <code.crafted100gmail.com>
2026-04-25 03:44:03 +0530

    added user class {bugs need to be fixed}

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ class song{
         int playCount;
     
     public:
-        song(){
-        }
+        song(){}
         song(std::string title,std::string artist, float duration){
             this -> title = title;
             this -> artist = artist;
@@ -39,6 +38,7 @@ class playlist{
         int songCount;
     
     public:
+        playlist(){}
         playlist(std::string playListName){
             this -> playListName = playListName;
             songCount = 0;
@@ -64,16 +64,46 @@ class playlist{
             
         }
 };
+class user{
+    private:
+        std::string username;
+        playlist playlists[5];
+        int playlistCount;
+    
+    public:
+        user(std::string username){
+            this -> username = username;
+            playlistCount = 0;
+        }
+        void addPlaylist(playlist &obj){
+            if(playlistCount < 2){
+                playlists[playlistCount] = obj;
+                playlistCount++;
+            }
+            else{
+                std::cout<<"MAX playlist limit reached!"<<std::endl;
+            }
+        }
+        void createPlaylist(std::string playlistName){
+            playlist playlist(playlistName);
+            addPlaylist(playlist);
+            std::cout<<"Playlist Created!"<<std::endl;
+        }
+
+        void display(){
+            std::cout <<"*****"<<username<<"*****"<<std::endl;
+            for (int i = 0; i != playlistCount; i++){
+                playlists[i].display();
+            }
+        }
+};
 int main(){
     song song1("Bulleya", "Papon", 357);
     song song2("Sach keh raha hai", "KK", 329);
 
-    playlist playlist1("3AM Songs");
+    user user1("Amish");
+    user1.createPlaylist("3 AM Songs");
+    user1.createPlaylist("PHONK");
+    user1.createPlaylist("AIZEN");
 
-    playlist1.addSong(song1);
-    playlist1.addSong(song2);
-
-    playlist1.display();
-    std::cout<<song1.getPlayCount()<<std::endl;
-    std::cout<<song2.getPlayCount()<<std::endl;
 }
