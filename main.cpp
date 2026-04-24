22191e5f51f8faae56251da614b1f7832f3a0b9d
Amish <code.crafted100gmail.com>
2026-04-25 03:05:43 +0530

    added playlist class

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ class song{
         int playCount;
     
     public:
+        song(){
+        }
         song(std::string title,std::string artist, float duration){
             this -> title = title;
             this -> artist = artist;
@@ -30,16 +32,48 @@ class song{
             return playCount;
         }
 };
+class playlist{
+    private:
+        std::string playListName;
+        song songs[10];
+        int songCount;
+    
+    public:
+        playlist(std::string playListName){
+            this -> playListName = playListName;
+            songCount = 0;
+        }
 
+        void addSong(song &obj){
+            if(songCount < 10){
+                songs[songCount] = obj;
+                songCount++;
+                std::cout <<"Song added!"<<std::endl;
+            }
+            else{
+                std::cout<<"MAX song limit reached!"<<std::endl;
+            }
+        }
+
+        void display(){
+            std::cout<<"-------------"<<playListName<<"-------------"<<std::endl;
+            
+            for (int i = 0; i != songCount; i++){
+                songs[i].display();
+            }
+            
+        }
+};
 int main(){
     song song1("Bulleya", "Papon", 357);
     song song2("Sach keh raha hai", "KK", 329);
 
-    song1.display();
-    song2.display();
-    song1.play();
-    
-    std::cout<<"Play Count: "<< song1.getPlayCount()<<std::endl;
-    std::cout<<"Play Count: "<< song2.getPlayCount()<<std::endl;
+    playlist playlist1("3AM Songs");
 
+    playlist1.addSong(song1);
+    playlist1.addSong(song2);
+
+    playlist1.display();
+    std::cout<<song1.getPlayCount()<<std::endl;
+    std::cout<<song2.getPlayCount()<<std::endl;
 }
