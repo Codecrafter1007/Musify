4f7d1033e175abea00e65fbc32d95ca730ee7f06
Amish <code.crafted100gmail.com>
2026-04-28 02:22:03 +0530

    successfully rebuilt the code in python!

--- Main.py
+++ Main.py
@@ -17,9 +17,57 @@ class Song:
     def getPlaycount(self):
         return self.playcount
 
+class playlist:
+    def __init__(self, playlistName):
+        self.playlistName = playlistName
+        self._songs = []
+        self.songcount = 0
+    
+    def addSong(self, song):
+        self._songs.append(song)
+        self.songcount += 1
+        print(f"{song.title} added! \n")
+    
+    def display(self):
+        print(f"-------------{self.playlistName}-------------\n")
+        
+        for i in self._songs:
+            i.display()
+
+class user:
+    
+    def __init__(self, userName):
+        self.userName = userName
+        self.playlistCount = 0
+        self._playlists = []
+
+    def addPlaylist(self, playlist):
+        self._playlists.append(playlist)
+                
+    def createPlaylist(self, playlistName):
+        playlistName = playlist(playlistName)
+        self.addPlaylist(playlistName)
+        print("Playlist created! \n")
+    
+    def add_song_to_playlist(self, playlist_number, song):
+        self._playlists[playlist_number - 1].addSong(song)
+        
+    
+    def display(self):
+        print(f"***************{self.userName}****************\n")
+
+        for i in self._playlists:
+            i.display()
+        
+    
+    
 #main
 song1 = Song("Bulleya", "Papon", 357)
 song2 = Song("Sach keh raha hai", "KK", 329)
 
-song1.play()
-print(song1.getPlaycount())  # will print 1, not 0
+user1 = user("Amish")
+user1.createPlaylist("sad songs")
+user1.add_song_to_playlist(1, song1)
+user1.add_song_to_playlist(1, song2)
+user1.display()
+
