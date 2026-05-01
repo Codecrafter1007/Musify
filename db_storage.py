05fa8c3d917cc1620ba4c3c52b7d72b38c9db2dd
Amish <code.crafted100gmail.com>
2026-05-02 00:12:00 +0530

    added load and save functionality

---
+++ db_storage.py
@@ -0,0 +1,44 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+from song import Song
+load_dotenv()
+
+def db_load_songs():
+    conn = psycopg2.connect(
+        host = "localhost",
+        database = "musify",
+        user = "postgres",
+        password = os.getenv("DB_PASSWORD")
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM songs")
+    rows = cursor.fetchall() #fetches entire data from the table
+    conn.close()
+    
+    songs = []
+    for row in rows:
+        song = Song(row[1], row[2], row[3])
+        song.playcount = row[4]
+        songs.append(song)
+    return songs
+
+def db_save_songs(songs):
+    conn = psycopg2.connect(
+        host = "localhost",
+        database = "musify",
+        user = "postgres",
+        password = os.getenv("DB_PASSWORD")
+    )
+    
+    cursor = conn.cursor()
+    
+    for song in songs:
+        cursor.execute("INSERT INTO songs(title, artist , duration , playcount) VALUES(%s , %s , %s , %s) ON CONFLICT (title) DO NOTHING;",
+                       (song.title, song.artist, song.duration, song.playcount))
+        
+    conn.commit()
+    conn.close()
+songs = db_load_songs()
+db_save_songs(songs)
